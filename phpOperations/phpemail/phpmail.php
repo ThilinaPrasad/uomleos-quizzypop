605a21c366f4f2eea93e33b41d1110b40058740f
Thilina Prasad <25478761+ThilinaPrasad@users.noreply.github.com>
2018-02-01 18:04:01 +0530

    updated

--- phpOperations/phpemail/phpmail.php
+++ phpOperations/phpemail/phpmail.php
@@ -7,13 +7,13 @@ function sendMail($email,$subject, $bodyContent)
     $mail->isSMTP();                                   // Set mailer to use SMTP
     $mail->Host = 'mail.uomleos.org';                    // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                            // Enable SMTP authentication
-    $mail->Username = 'quizzypop@uomleos.org';          // SMTP username
-    $mail->Password = 'bestleos2017';            // SMTP password
+    $mail->Username = '<Email Goes Here>';          // SMTP username
+    $mail->Password = '<Password Goes Here>';            // SMTP password
     $mail->SMTPSecure = 'ssl';                         // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                 // TCP port to connect to
 
-    $mail->setFrom('quizzypop@uomleos.org', 'QuizzyPop');
-    $mail->addReplyTo('uomleoclub@gmail.com', 'UoM Leos');
+    $mail->setFrom('<Email>', '<Sender Name>');
+    $mail->addReplyTo('<Reply Email>', '<Reply Emai Name>');
     $mail->addAddress($email);   // Add a recipient
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
